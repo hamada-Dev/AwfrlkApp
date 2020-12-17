58d249c49a5082e06872ab088f740d945398ab08
hamada-Dev <h_diver1@yahoo.com>
2020-12-17 23:20:46 +0200

    data edit

--- app/asem.php
+++ app/asem.php
@@ -1,2 +1,11 @@
 asesm ahmed sgaiobo congrateulation
 from hamada diver 
+
+////////////////////////////// hamada date //////////////////////////////////
+ @php
+$last2year = time() - (2 * 12 * 30 * 24 * 60 * 60);
+$start_date = date('Y-m-d', $last2year);
+@endphp
+<input id='start_date' type="date" name="start_date" id="" class="form-control"
+	value="{{ request()->start_date ?? $start_date }}">
+////////////////////////////// hamada date //////////////////////////////////
